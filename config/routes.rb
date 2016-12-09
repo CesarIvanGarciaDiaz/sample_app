1714322de90e6cb47c16a26dfca063f80d25be8c
CesarIvanGarciaDiaz <inge__garcia@hotmail.com>
2016-12-09 11:36:16 -0600

    Finish static pages"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-root 'application#hello'
+  get  'static_pages/about'
+
+  get 'static_pages/contact'
+
 end
