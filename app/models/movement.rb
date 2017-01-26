e405d911f91bf600087414c28d34177c6569b9ae
ZSoJes <gomezsotelojuan@gmail.com>
2017-01-26 11:31:20 -0600

    movimiento

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -12,13 +12,13 @@ class Movement < ApplicationRecord
   )}
 
   validates :reference, uniqueness: {:message => "El número de referencia ya ha sido utilizado"}, allow_blank: true
-  validates :deposit, :withdrawal, :numericality => { :greater_than_or_equal_to => 0, :message => "La cantidad ingresada debe numerica y sin signos"  }
+  validates :deposit, :withdrawal, :numericality => { :greater_than_or_equal_to => 0, :message => "La cantidad ingresada debe ser numerica y sin signos"  }
   validates :concepto_de_pago, presence: {:message => "debe ser llenado"}
   validates :date, presence: {:message => "Debe indicarse una fecha de registro"}
 
-  before_create do
-    self.withdrawal = self.withdrawal * -1 if self.withdrawal >= 0
-  end
+  # before_create do
+  #   self.withdrawal = self.withdrawal * -1 if self.withdrawal >= 0
+  # end
 
 
   # has_many :movement_defineds
